2b9492c0a1e6685eb208fb7d02c2c1b3e66516c8
Sorakime <69241694+Sorakime@users.noreply.github.com>
2022-02-10 21:52:01 +0900

    iroiro........takusan........

--- script/common.js
+++ script/common.js
@@ -25,15 +25,15 @@ window.onload = () => {
     </div>
     <div class="footerColumn">
       <p><a href="/about/dept/" class="footer-bigger">部署一覧</a></p>
-      <p><a href="/about/dept/thinkfont/">thinkFont</a></p>
-      <p><a href="/about/dept/thinkos/">thinkOS</a></p>
+      <p><a href="/project/thinkfont/">thinkFont</a></p>
+      <p><a href="/project/thinkos/">thinkOS</a></p>
       <p><a href="/about/dept/web/">Web開発</a></p>
       <p><a href="/about/dept/material/">素材等作成</a></p>
     </div>
     <div class="footerColumn">
       <p><a href="/project/" class="footer-bigger">プロジェクト一覧</a></p>
-      <p>thinkFont</p>
-      <p>thinkOS</p>
+      <p><a href="/project/thinkfont">thinkFont</a></p>
+      <p><a href="/project/thinkos">thinkOS</a></p>
       <p><a href="https://sorakime.github.io/mncr/">monochrome Project.</a></p>
     </div>
     <div class="footerColumn">
