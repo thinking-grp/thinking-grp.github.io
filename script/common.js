bd72e3c8934e0be939f236cac79f7037a52a0588
EARTH <92440881+EARTH-01111@users.noreply.github.com>
2022-08-21 05:46:10 +0900

    Update common.js

--- script/common.js
+++ script/common.js
@@ -36,9 +36,8 @@ window.onload = () => {
     <div class="footerColumn">
       <p><a href="/agreement">プライバシーポリシーと利用規約</a></p>
       <div class="year-s">
-        <p>&copy; &#160;<div>2022-</div><div id="year"></div></p>
+        <p>Copyright &copy; &#160;<div>2022-</div><div id="year"></div>thinking All rights reserved.</p>
       </div>
-      <p>thinking All rights reserved.</P>
     </div>
   </footer>
   `;
