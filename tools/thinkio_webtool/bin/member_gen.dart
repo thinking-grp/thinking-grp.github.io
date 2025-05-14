18f02e9d4739a8f0ec4a2de16d78626c7b971902
佐藤陽花 <as.he.that.doth.serve@gmail.com>
2025-05-14 13:27:59 +0900

    member_gen.dart の更新

--- tools/thinkio_webtool/bin/member_gen.dart
+++ tools/thinkio_webtool/bin/member_gen.dart
@@ -3,7 +3,7 @@ import "dart:convert";
 import "package:yaml/yaml.dart";
 import "package:color/color.dart";
 
-const bool deploy = false;
+const bool deploy = true;
 
 void main() {
   Uri curr = Platform.script;  // /tools/thinkio_webtool/bin/member_gen.dart
