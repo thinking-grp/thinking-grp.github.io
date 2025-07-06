38bb539f8bdbb9133ac7a6a5d0a6dddd5fc0ba24
佐藤陽花 <as.he.that.doth.serve@gmail.com>
2025-07-07 05:46:43 +0900

    ud: テンプレート使用化

--- tools/thinkio_webtool/bin/member_gen.dart
+++ tools/thinkio_webtool/bin/member_gen.dart
@@ -8,15 +8,19 @@ void main() {
   Uri curr = Platform.script;  // /tools/thinkio_webtool/bin/member_gen.dart
   Uri base = curr.replace(pathSegments: curr.pathSegments.take(curr.pathSegments.length - 4).toList()); // /
   Uri din = base.replace(pathSegments: base.pathSegments.followedBy(<String>["data", "member_profiles.yaml"]).toList()); // /data/member_profiles.yaml
+  Uri tin = base.replace(pathSegments: base.pathSegments.followedBy(<String>["template", "member.thtm"]).toList()); // /template/member.thtm
+  Uri sin = base.replace(pathSegments: base.pathSegments.followedBy(<String>["template", "member_profiles.css"]).toList()); // /template/member.css
   Uri dout = base.replace(pathSegments: base.pathSegments.followedBy(<String>["about", deploy ? "member.html" : "member_generated.html"]).toList()); // /about/member_generated.html (dryrun), /about/member.html (deploy)
 
-  File fin = File.fromUri(din);
+  File find = File.fromUri(din);
+  File fint = File.fromUri(tin);
+  File fins = File.fromUri(sin);
   File fout = File.fromUri(dout);
   if(!fout.existsSync()){
     fout.createSync();
   }
   try{
-    generate(fin, fout);
+    generate((data: find, html: fint, style: fins), fout);
   } on YamlSchemaViolationError catch(e, t) {
     print(e);
     print(t);
