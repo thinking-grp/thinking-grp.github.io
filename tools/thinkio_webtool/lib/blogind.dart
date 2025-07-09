f53782fc1a6878d246c8a596a5ca7b33aae19490
halka9000stg <as.he.that.doth.serve@gmail.com>
2025-07-09 14:39:10 +0900

    add: blogタグ追加; ud: YAMLパースコンストラクタ

--- tools/thinkio_webtool/lib/blogind.dart
+++ tools/thinkio_webtool/lib/blogind.dart
@@ -5,23 +5,38 @@ import "package:thinkio_webtool/xlib.dart";
 
 final DateFormat fmt = DateFormat("yyyy年MM月dd日", "ja-JP");
 
+extension type BlogTag._(String name){
+  BlogTag(String tagName):
+    this.name = BlogTag.validate(tagName);
+  //ToDo: add static param of RegExp
+  //               for tag format
+  //      and make validate use the param
+  //               as matcher
+  static String validate(String input)
+    => true;
+}
+
 class BlogRec implements Buildable<BlogRec> {
   final String title;
   final DateTime postedAt;
   final DateTime lastUpdatedAt;
   final String author;
+  final List<BlogTag> tags;
   final Uri path;
   final Uri image;
   final String lead;
   
-  static Uri defaultImage = Uri.parse("");
+  static Uri defaultImage = Uri.parse("/image/thinking-img.jpg");
   
-  BlogRec({required this.title, required DateTime postedAt, DateTime? lastUpdatedAt, required this.author, required this.path, Uri? image, this.lead = ""}):
+  BlogRec({required this.title, required DateTime postedAt, DateTime? lastUpdatedAt, required this.author, required this.path, Uri? image, Iterable<BlogTag>? tags, this.lead = ""}):
       this.postedAt = postedAt,
-      this.lastUpdatedAt = lastUpdatedAt ?? postedAt
-      this.image = image ?? BlogRec.defaultImage;
+      this.lastUpdatedAt = lastUpdatedAt ?? postedAt,
+      this.image = image ?? BlogRec.defaultImage,
+      this.tags = tags?.toList() ?? <BlogTag>[];
   
-  factory BlogRec.fromYaml(YamlMap yaml) {}
+  factory BlogRec.fromYaml(YamlMap yaml) {
+    List<String> _ = yaml.hasKeys(requires: <String>["title", "postedAt", "author", "path"], optionals: <String>["lastUpdatedAt", "image", "tags", "lead"]);
+  }
   
   @override
   int compareTo(BlogRec other, {bool byName = false, bool asc = true}){
