bdffb7ea5dde864093d90048cb10fb986dff46ca
halka9000stg <as.he.that.doth.serve@gmail.com>
2025-08-04 13:43:43 +0900

    fix: intl locale initialize

--- tools/thinkio_webtool/lib/blogind.dart
+++ tools/thinkio_webtool/lib/blogind.dart
@@ -3,7 +3,10 @@ import "package:yaml/yaml.dart";
 import "package:intl/intl.dart";
 import "package:thinkio_webtool/xlib.dart";
 
-final DateFormat fmt = DateFormat("yyyy年MM月dd日", "ja-JP");
+final get DateFormat fmt {
+  initializeDateFormatting("ja-JP"); 
+  return DateFormat("yyyy年MM月dd日", "ja-JP");
+}
 
 extension type BlogTag._(String name){
   BlogTag(String tagName):
