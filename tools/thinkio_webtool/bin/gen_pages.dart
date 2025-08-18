6146e42b8276db7811ea3ad2d84c0603d7a483eb
halka9000stg <as.he.that.doth.serve@gmail.com>
2025-08-18 09:08:15 +0900

    add: データ取得(ロード)プロセスの統合用にLoadedDataを追加; ud: sitemap関連のロードを統合

--- tools/thinkio_webtool/bin/gen_pages.dart
+++ tools/thinkio_webtool/bin/gen_pages.dart
@@ -32,7 +32,13 @@ void main() {
   File fout_b = File.fromUri(dout_b);
   
   try{
-    generate_sitemap(pf_b.base, pf_b.data);
+    final ld = LoadedData(
+        base: Directory.fromUri(base),
+        blogPath: <String>["data", "blog_index.yaml"],
+        memberPath: <String>["data", "member_profiles.yaml"]
+      );
+    
+    generate_sitemap(ld);
     generate_member(pf_m, fout_m);
     generate_blog_index(pf_b, fout_b);
   } on YamlSchemaViolationError catch(e, t) {
--- tools/thinkio_webtool/lib/generate.dart
+++ tools/thinkio_webtool/lib/generate.dart
@@ -1,18 +1,34 @@
 import "dart:io";
+import "package:yaml/yaml.dart" show YamlMap;
 import "package:thinkio_webtool/mprof.dart";
 import "package:thinkio_webtool/blogind.dart";
 import "package:thinkio_webtool/templater.dart";
 import "package:thinkio_webtool/sitemap.dart";
 import "package:thinkio_webtool/xlib.dart";
 
+class LoadedData {
+  final Directory base;
+  final Iterable<BlogRec> blogsAll;
+  final Iterable<MemberProfile> members;
+  
+  LoadedData({required Directory base, required Iterable<String> blogPath, required Iterable<String> memberPath}):
+    this.base = base,
+    this.blogsAll = LoadedData.load<BlogRec>(base, blogPath, BlogRec.fromYaml, needSort: true, reverse: true),
+    this.members = LoadedData.load<MemberProfile>(base, memberPath, MemberProfile.fromYaml, needSort: true, filterItem: (MemberProfile mp) => mp.current);
+  
+  Iterable<BlogRec> blogsLimit(int count)
+    => this.blogsAll.take(count);
+  
+  static Iterable<H> load<H extends Buildable>(Directory base, Iterable<String> dataPath, H Function(YamlMap) fromYaml, {bool needSort = false, bool reverse = false, int? limit, bool Function(H)? filterItem})
+    => base.cd<File>(dataPath).readAsStringSync().construct<H>(fromYaml, needSort: needSort, reverse: reverse, limit: limit, filterItem: filterItem);
+  }
+}
 
-void generate_sitemap(Directory base, Iterable<String> dataPath){
+void generate_sitemap(LoadedData ld){
   print("generate:generate_sitemap()");
-  String data = base.cd<File>(dataPath).readAsStringSync();
-  File out = File.fromUri(base.uri.cd(<String>["sitemap.xml"]));
-  print("base.uri: Uri");
-  print(base.uri.pathSegments);
-  genSitemap(base.uri, out, data.construct<BlogRec>(BlogRec.fromYaml, needSort: true, reverse: true));
+  Uri u = ld.base.uri;
+  File out = File.fromUri(u.cd(<String>["sitemap.xml"]));
+  genSitemap(u, out, ld.blogsAll);
 }
 void generate_member(PageFiles fin, File fout){
   final tlr = Templater(fin.base, fin.html, fout);
